1638d3b84489a6cc7326583ed20328ad4cffcd99
wanghualin <wanghualin@yourangroup.com>
2021-02-06 15:11:08 +0800

    create project

---
+++ ffmpeg_encode_frame.cpp
@@ -0,0 +1,11 @@
+#include "ffmpeg_encode_frame.h"
+
+FFmpegEncodeFrame::FFmpegEncodeFrame(const VideoParams &params)
+{
+    
+}
+
+bool FFmpegEncodeFrame::Encode(void *data, int size, int type)
+{
+
+}
---
+++ ffmpeg_encode_frame.h
@@ -0,0 +1,39 @@
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavutil/opt.h>
+#include <libavutil/imgutils.h>
+}
+
+//https://ffmpeg.org/doxygen/trunk/encode_video_8c-example.html
+class FFmpegEncodeFrame
+{
+public:
+    struct VideoParams
+    {
+        /* put sample parameters */
+        int bit_rate = 400000;
+        /* resolution must be a multiple of two */
+        int width = 352;
+        int height = 288;
+        /* frames per second */
+        AVRational time_base{1,25};
+        AVRational framerate{25,1};
+        /* emit one intra frame every ten frames
+        * check frame pict_type before passing frame
+        * to encoder, if frame->pict_type is AV_PICTURE_TYPE_I 
+        * then gop_size is ignored and the output of encoder
+        * will always be I frame irrespective to gop_size
+        */
+        int gop_size = 10;
+        int max_b_frames = 1;
+        int pix_fmt = AV_PIX_FMT_YUV420P;
+    };
+    explicit FFmpegEncodeFrame(const VideoParams &params);
+
+    bool Encode(void *data, int size, int type);
+
+private:
+    const AVCodec *codec_ = nullptr;
+    AVCodecContext *ctx_ = nullptr;
+};
